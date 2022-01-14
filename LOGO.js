08093f51a0971fbccd28ff83373b2d2ba892e9af
Josué Barros <frajolla27@gmail.com>
2022-01-14 16:55:09 -0300

    <feat>(creat loadg logo)

---
+++ LOGO.js
@@ -0,0 +1,15 @@
+Mud=MediaStreamAudioSourceNode(//autorizacao se prescisar
+  )
+000000ibwth = onabort.ibwth.onabort.m
+oi = this.undefined(q.ed.ewoq()){
+  const otcuLehrDunco = yield.ibwth.onw
+} 
+
+ua  fi aoe  er   du rri screens  se n´ e
+ no  d [r sd i  u ue r[ s s r si e wiie ´t-´s=-dr psi se r ´[ra- r aue r aráod oadodj e8 r  au rd á wr rpa  af a r prei df aorparp aroar ah uu fa d arpai  a aw [ ´- wi is sa a aw ois g´a ÌP P Uf somfp pw os j sh jd fj ms´e epu wk wppps   `P`O  Poka os s p oaosi ik of ag a iua ur p aóa pa fp ásfá fpa s[a p ss o a ai i h dy fiawo wpw epe´pa p os d iu ais tnr´´qQ PQ OAJ  SPAS FAP W I  A Plmp o ol  P l   toi I JOO i ii   dp g s d isu dy oh ´r ro  [dp gskds fhk  d ps sps sp]]]]]].{
+     s j jsud ogspe g oe iuw  aç spçsf g als k g ee pw iuj oa isf  (ikj so dj u ig eo  ow w o go P ( sd spd fpg çpP i si  fao psa ok i I N Oi u odp  dj d O DPdi aos ap fp pao au ai d afo ()8d skj  dj d gila ow wp ruh  s f iagk oe iae uhhg as ig w w o eo oaet it oa u uy u di fi ad ofi iw hw  yr iet it i weiu kmsjn zhx zjx jv k js  ifk oais gsiajs iks ri e uiá suds fhua y s fowe  w e ikt yh ua hg g dj ypi i a pasd ga´ge i (( s j j go pw p et´p[ w´p ertpo i u ya us di fao iose ir t ti toas y ig o e w pto ioi sisf {si  sudfo 
+     ois i di ig os ep w´et pwpe ptw t us kfj i seu triw towp os  p gs io eoe i w o oso fg pgo seouisirois ttit ipw opo ps rfo ({
+        i sj  u ue rto wpi eort   bs u d so goe ieutyi u s  u idi g soe eiiu wi  ui sj dgi eh ie is sd f giuieu t  sto  jn xmcjvjb  b okd ik s v s k xi kxvj nvih bvuuaj df k xbnoiv osi of{{()=>
+         i ai i si faow o topw iot iuh b as gp xp px bzoxb osop oq piw yg riw tqw oo   oisd p fgp p xo ziu ucxy ivsi u osg posos p doiu go xo  i obiodi is dyg osp po }()=>i si  si dfog pe po ow iunb aslgna ps og opahpa is u vyutv ug i as ig olkm xi no ib z s oalmn h s jg l xi kjj ujd uj s uaiu iuayhg vzco kj  jsd asjh}
+     })}]))))
+ }
